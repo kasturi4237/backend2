096188123fd8ce2191b440bdf0f466f4a0e1c1fe
kasturi4237 <kastum7890@gmail.com>
2025-03-25 20:11:08 +0530

    update2

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -52,7 +52,7 @@ const registerUser = asyncHandler( async (req, res) => {
     if (existedUser) {
         throw new ApiError(409, "User with email or username already exists")
     }
-    console.log(req.files);
+    //console.log(req.files);
 
     const avatarLocalPath = req.files?.avatar[0]?.path;
     //const coverImageLocalPath = req.files?.coverImage[0]?.path;
@@ -494,4 +494,3 @@ export {
     getUserChannelProfile,
     getWatchHistory
 }
-    
